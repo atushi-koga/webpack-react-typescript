5587a8cce326a58e20b92339de7217210bbe3b8f
koga <koga@wh-plus.com>
2020-09-17 23:46:38 +0900

    feat: 画面表示用のHTML, JSを作成

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {HelloWorld} from './sub';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <HelloWorld name="okutani" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
